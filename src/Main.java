9ff6549cb2ced55261382350a649a43467f13d2b
clarklark <3137695115@qq.com>
2025-03-11 15:40:00 +0800

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello github!");
+        System.out.println("Hello github3!");
+        System.out.println("Hello github!3");
     }
 }
