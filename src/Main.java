adb7784b6656f430a2aae6d5b9337f45d2495ea2
clarklark <3137695115@qq.com>
2025-03-12 17:09:56 +0800

    hot-fix first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello github!");
+        System.out.println("Hello github!4");
+        System.out.println("Hello hot-fix");
     }
 }
